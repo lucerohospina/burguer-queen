d396a5cbceab57ea08b1b8dbc1e3419986700800
eliza29 <elizaseguraf@gmail.com>
2018-03-11 18:25:07 -0500

    header

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import burger from './burger.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <nav className="navbar navbar-light bg-light justify-content-center">
+          <a className="navbar-brand main-title text-warning" href="#">
+          <img src={burger} width="45" height="45" className="d-inline-block align-top" alt="burger"/>
+              Burger Queen
+          </a>
+         </nav>
       </div>
-    );
-  }
-}
-
-export default App;
+        );
+      }
+    }
+    
+    export default App;
